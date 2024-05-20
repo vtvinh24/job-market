62fad04195a980cafa75819fbdde4c2e4ff8172d
Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-05-20 07:30:50 +0700

    Add react-router dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>
 );
