8a9175633342c2ce4b3c7d21fb0d964c811610fe
MeoHoang176788 <hoangmnhe176788@fpt.edu.vn>
2024-05-20 15:01:58 +0700

    Update Components HomePage

---
+++ src/HomeScreen/HomeComponents/Components/CarouselComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+function CarouselComponent() {
+  return (
+    <div className='carousel-container'>
+      <Carousel data-bs-theme="dark" className='Carousel'>
+        <Carousel.Item>
+          <div className="p-3 border bg-light text-center content-boxs">Content Box 1</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="p-3 border bg-light text-center content-boxs">Content Box 2</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="p-3 border bg-light text-center content-boxs">Content Box 3</div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default CarouselComponent;
---
+++ src/HomeScreen/HomeComponents/Components/HotNew.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HotNew() {
+  return (
+    <div className='hot-news'>
+      <div className="mt-4">
+        <h2>Hot News</h2>
+        <div className="p-3 border bg-light text-center new-box">New Box</div>
+      </div>
+    </div>
+  );
+}
+
+export default HotNew;
