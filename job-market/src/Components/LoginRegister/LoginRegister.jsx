7c92a433b6f7577a6e3e1bbab2761ba36491c7ac
DUCDMHE176494 <ducdmhe176494@fpt.edu.vn>
2024-05-23 10:31:43 +0700

    Finish basic form login

--- job-market/src/Components/LoginRegister/LoginRegister.jsx
+++ job-market/src/Components/LoginRegister/LoginRegister.jsx
@@ -6,6 +6,23 @@ const LoginRegister =() => {
             <div className='form-box login'>
                 <form action="">
                     <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text"
+                        placeholder='Username'required />
+                    </div>
+                    <div className="input-box">
+                        <input type="password"
+                        placeholder='Password'required />
+                    </div>
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />
+                        Remember me</label>
+                        <a href="#">Forgot password?</a>
+                        <button type = "submit">login</button>
+                    <div className='register-link'>
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                    </div>
                 </form>
             </div>
         </div>
