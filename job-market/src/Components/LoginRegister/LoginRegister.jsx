980ded08837029542f958b001ee741c6b998a4c1
DUCDMHE176494 <ducdmhe176494@fpt.edu.vn>
2024-05-23 13:54:29 +0700

    fix div in LoginRegister.jsx

--- job-market/src/Components/LoginRegister/LoginRegister.jsx
+++ job-market/src/Components/LoginRegister/LoginRegister.jsx
@@ -15,7 +15,7 @@ const LoginRegister =() => {
     return(
         <div className={`wrapper ${action}`}>
             <div className='form-box login'>
-                <form action="active">
+                <form action="">
                     <h1>Login</h1>
                     <div className="input-box">
                         <input type="text"
@@ -31,10 +31,10 @@ const LoginRegister =() => {
                         <label><input type="checkbox" />
                         Remember me</label>
                         <a href="#">Forgot password?</a>
+                        </div>
                         <button type = "submit">login</button>
                     <div className='register-link'>
                         <p>Don't have an account? <a href="#" onClick={registerlink}>Register</a></p>
-                    </div>
                     </div>
                 </form>
             </div>
@@ -59,12 +59,13 @@ const LoginRegister =() => {
                     <div className="remember-forgot">
                         <label><input type="checkbox" />
                         I agree to the terms & conditions</label>
+                    </div>    
         
                         <button type = "submit">Resister</button>
                     <div className='register-link'>
                         <p>Already have an account? <a href="#"onClick={loginlink}>Login</a></p>
                     </div>
-                    </div>
+                    
                 </form>
             </div>
         </div>
