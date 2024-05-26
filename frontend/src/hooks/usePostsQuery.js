d22bc9284d12abf728a473b4e9d367c551fd6ca5
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-05-26 07:35:21 +0700

    Update Forum page

--- frontend/src/hooks/usePostsQuery.js
+++ frontend/src/hooks/usePostsQuery.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/api';
 
 const usePostsQuery = () => {
     const [posts, setPosts] = useState([]);
