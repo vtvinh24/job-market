b657bd914e00dee7c52fe6098896db84044d426d
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-05-27 09:16:00 +0700

    Update usePostsQuery.js

--- frontend/src/hooks/usePostsQuery.js
+++ frontend/src/hooks/usePostsQuery.js
@@ -1,36 +1,30 @@
-import axios from 'axios';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = "http://localhost:8000/api";
 
-const usePostsQuery = (sortBy = null) => {
-const usePostsQuery = (sortBy = null) => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const usePostsQuery = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            // await new Promise((resolve) => setTimeout(resolve, 5000));
-            try {
-                let url = `${API_URL}/posts`;
-                // if (sortBy) {
-                //     url += `?_sort=${sortBy}`;
-                // }
-                const response = await axios.get(url);
-                setPosts(response.data);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const url = `${API_URL}/posts`;
+        const response = await axios.get(url);
+        setPosts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
 
-        fetchPosts();
-    }, [sortBy]);
+    fetchPosts();
+  }, []);
 
-    return { posts, loading, error };
+  return { posts, loading, error };
 };
 
 export default usePostsQuery;
