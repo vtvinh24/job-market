4c883bac0779a5a44d90ba264ea0ba427e1c7d22
KhanhNQ-HE181237 <169059971+KhanhNQ-HE181237@users.noreply.github.com>
2024-05-27 15:58:10 +0700

    Addjob

--- frontend/src/pages/Addjob.jsx
+++ frontend/src/pages/Addjob.jsx
@@ -3,24 +3,32 @@ import React, { useState } from 'react';
 const Addjob = () => {
   const [jobTitle, setJobTitle] = useState('');
   const [company, setCompany] = useState('');
+  const [address, setAddress] = useState('');
   const [description, setDescription] = useState('');
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log('Job Submitted:', { jobTitle, company, description });
+    console.log('Job Submitted:', { jobTitle, company,address, description });
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      
+
+        
       <div>
-        <label>Job Title:</label>
+        <h1>Add Job</h1>
+      </div>
+      <div>
+        <label>Job's name:</label>
         <input
           type="text"
           value={jobTitle}
           onChange={(e) => setJobTitle(e.target.value)}
         />
-      </div>
+      </div><br></br>
       <div>
         <label>Company:</label>
         <input
@@ -28,7 +36,19 @@ const Addjob = () => {
           value={company}
           onChange={(e) => setCompany(e.target.value)}
         />
-      </div>
+      </div><br></br>
+      <div>
+      <label>Address:</label>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div><br></br>
+      <div>
+        <label>Skills:</label>
+
+      </div><br></br>
       <div>
         <label>Description:</label>
         <textarea
