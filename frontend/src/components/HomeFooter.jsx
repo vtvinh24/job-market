39fd3913f0f780ad1eb120a0059b5cca6197df58
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-05-29 07:48:55 +0700

    Remove duplicate pages and make some minor changes

---
+++ frontend/src/components/HomeFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeFooter() {
+  return (
+    <footer className="text-end p-3">
+      <div className="text-muted">Help Center</div>
+    </footer>
+  );
+}
+
+export default HomeFooter;
