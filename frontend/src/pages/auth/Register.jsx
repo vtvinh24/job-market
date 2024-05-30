e7b699643c8bee78ea21f6243b633e1a383e6855
MeoHoang176788 <hoangmnhe176788@fpt.edu.vn>
2024-05-30 13:23:26 +0700

    Revert "Fixing Navbar"
    This reverts commit 0782f8b1f150e4f7d5112e5940b8b2e20c31a365.

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./../../assets/css/LoginRegister.css";
-// import { FaUser, FaLock } from "react-icons/fa";
-// import { MdAlternateEmail } from "react-icons/md";
+import { FaUser, FaLock } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
 
 const Register = () => {
     return (
@@ -11,15 +11,15 @@ const Register = () => {
                 <h1>Register</h1>
                 <div className="input-box">
                     <input type="text" placeholder='Username' required />
-                    {/* <FaUser className='icon' /> */}
+                    <FaUser className='icon' />
                 </div>
                 <div className="input-box">
                     <input type="email" placeholder='Email' required />
-                    {/* <MdAlternateEmail className='icon' /> */}
+                    <MdAlternateEmail className='icon' />
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder='Password' required />
-                    {/* <FaLock className='icon' /> */}
+                    <FaLock className='icon' />
                 </div>
                 <div className="remember-forgot">
                     <label>
