2f765d7e610a0bcaca806534325b6a02800d4391
KhanhNQ-HE181237 <169059971+KhanhNQ-HE181237@users.noreply.github.com>
2024-06-04 16:59:28 +0700

    Co-authored-by: HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,15 @@
 import "./App.css";
-import HomePage from "./pages/home/HomePage";
-import PageNotFound from "./pages/technical/PageNotFound";
-import Forum from "./pages/forum/Forum";
-import Market from "./pages/jobs/Market";
-import HomeGuest from "./pages/home/HomeGuest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeNavbar from "./components/HomeNavbar";
-import HomeFooter from "./components/HomeFooter";
-import AddPost from "./pages/forum/AddPost";
-import PostDetail from "./pages/forum/PostDetail";
-import AddJob from "./pages/jobs/AddJob";
-function App() {
-  return (
-    <BrowserRouter>
-      
-      <div className="content">
-        <Routes>
-          <Route exact path="/" element={<HomeGuest />} />
-          <Route exact path="/home" element={<HomePage />} />
-          <Route exact path="/forum" element={<Forum />} />
-          <Route exact path="/add-post" element={<AddPost />} />
-          <Route exact path="/add-job" element={<AddJob />} />
-          <Route exact path="/posts/:id" element={<PostDetail />} />
-          <Route exact path="/jobs" element={<Market />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Register />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
-     
-    </BrowserRouter>
+import EnlistJob from "./pages/jobs/EnlistJob.jsx";
+import React from 'react';
 
-  );
-}
-export default App;
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <EnlistJob />
+    </div>
+  )
+};
+
+export default App
--- frontend/src/pages/jobs/EnlistJob.jsx
+++ frontend/src/pages/jobs/EnlistJob.jsx
@@ -1,127 +1,189 @@
 import React from 'react';
 import '../../assets/css/EnlistJob.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import { Container, Button, Row, Form, Col } from 'react-bootstrap';
+import backgroundImg from '../../assets/img/Stole.jpg'; // Assuming your image is named company_logo.png
+import HomeNavbar from '../../components/HomeNavbar.jsx';
+import Footer from '../../components/HomeFooter.jsx';
 const EnlistJob = () => {
+
+/* 
+
+       COL 1 | COL 2 | COL 3
+ROW 1  TITLE 
+ROW 2  WORK TYPE | LOCATION
+ROW 3  TAGS
+
+
+*/
+
   return (
-    <div className="job-form-container">
-      <button className="back-button">Back</button>
-      <h1 className='header1'>Enlist a Job</h1>
-      <form className="job-form">
-        <div className="section">
-          <h2 className='header2'>General Information</h2>
-          <label>
-            Job's Title:
-            <input type="text" className="title" required />
-          </label>
-          <div>
-          <label>
-            Location:
-            <input type="text" name="location" />
-          </label>
-          <label>
-            Work type:
-            <select className="workType">
-              <option value="VND">VND</option>
-              <option value="USD">USD</option>
-              {/* Add more currencies as needed */}
-            </select>
-          </label>
-          </div>
-          <label>
-            Tags:
-            <input type="text" name="tags" placeholder="#online #it #ketoan ..." />
-          </label>
+    <>
+    <div className="App">
+        <HomeNavbar />
         </div>
-        
-        <div className="section">
-          <h2 className='header2'>Recruitment Settings</h2>
-          <label>
-            Max applications:
-            <input type="number" name="maxApplications" />
-          </label>
-          <label>
-            Approval method:
-            <input type="text" name="approvalMethod" />
-          </label>
-          <label>
-            Number of recruits:
-            <input type="number" name="numberOfRecruits" />
-          </label>
-          <div>
-          <label>
-            Start date:
-            <input type="date" name="startDate" />
-          </label>
-          <label>
-            End date:
-            <input type="date" name="endDate" />
-          </label>
-          </div>
-        </div>
-        
-        <div className="section">
-          <h2 className='header2'>Requirements</h2>
-          <label>
-            Document: CV
-            <input type="checkbox" name="documentCV" />
-          </label>
-        </div>
-        
-        <div className="section">
-          <h2 className='header2'>Compensation</h2>
-          <label>
-            Compensation Type:
-            <input type="text" className="compensationType" />
-            <label className="payWithBalance">
-            Pay with mJOB balance:
-            <input type="checkbox" name="payWithBalance" />
-          </label>
-          </label>
-          <div>
-          <label>
-            Amount:
-            <input type="number" name="amount" />
-            <select className="currency">
-              <option value="VND">VND</option>
-              <option value="USD">USD</option>
-              {/* Add more currencies as needed */}
-            </select>
-            per:
-            <select className="currency">
-              <option value="month">month</option>
-              <option value="week">week</option>
-              {/* Add more periods as needed */}
-            </select>
-            <input type="text" className="customIteration" placeholder="custom iteration/hours per day" />
-          </label>
-          
-          </div>
-        </div>
-        
-        <div className="section">
-          <h2 className='header2'>Additional Information</h2>
-          <label>
-            Description:
-            <div>
-            <textarea className="description"  placeholder='Enter Description'required></textarea>
-            </div>
-          </label>
-        </div>
-        
-        <div className="section">
-          <label>
-            Contact Info:
-            <input type="text" className="contactInfo" required />
-          </label>
-        </div>
-        
-        <button type="submit" className="enlist-button">Enlist</button>
-      </form>
-    </div>
+    <img src={backgroundImg} alt="Image" className="background-img"/>
+    
+      <Container className='container'>
+        <Form className='form'>
+          <Button href="/" className='back-button'>Back</Button>
+        <Row><h1 className='header1'>Enlist a Job</h1></Row>
+          <Row><h3 className='header2'>General Information</h3></Row>
+          <Row><Form.Group controlId="title">
+            <Form.Label>Job Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter job title" className='input' />
+          </Form.Group>
+          </Row>
+          <Row>
+            <Col md={3}>
+              <Form.Group controlId="workType">
+                <Form.Label>Work Type</Form.Label>
+                <Form.Control as="select">
+                  <option>Office</option>
+                  <option>Remote</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="location">
+                <Form.Label>Location</Form.Label>
+                <Form.Control type="text" placeholder="Enter location">
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+          <Col>
+             <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Control type="text" placeholder="Enter tags">
+                 </Form.Control>
+              </Form.Group>
+            </Col>
+        </Row>
+        <Row><h3 className='header2'>Recruitment Settings</h3></Row>
+        <Row>
+            <Col md={3}>
+              <Form.Group controlId="maxApplications">
+                <Form.Label>Max applications</Form.Label>
+                <Form.Control type="text" placeholder='Max Applications'>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col md={3}>
+              <Form.Group controlId="approvalmethod">
+                <Form.Label>Approval method</Form.Label>
+                <Form.Control as="select" placeholder='Approval method'>
+                  <option>Auto</option>
+                  <option>Manual</option>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col md={3}>
+              <Form.Group controlId="numberofrecruits">
+                <Form.Label>Number of recruits</Form.Label>
+                <Form.Control type="text" placeholder='Number of recruits'>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            </Row>
+            <Row>
+            <Col md={3}>
+              <Form.Group controlId="startdate">
+                <Form.Label>Start date</Form.Label>
+                <Form.Control type="date" placeholder='Start date'>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col md={3}>
+              <Form.Group controlId="enddate">
+                <Form.Label>End date</Form.Label>
+                <Form.Control type="date" placeholder='End date'>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            <Row>
+            <Col md={3}>
+              <Form.Group controlId="CV">
+                <Form.Label>Document: CV</Form.Label>
+                <Form.Control as="type"  placeholder='Document: CV'>
+               </Form.Control>
+              </Form.Group>
+            </Col>
+            </Row>
+            <Row><h3 className='header2'>Compensation</h3></Row>
+            <Row>
+            <Col md={3}>
+              <Form.Group controlId="compensationType">
+                <Form.Label>Compensation type</Form.Label>
+                <Form.Control as="select" placeholder='Compensation type'>
+                  <option>One-time</option>
+                  <option>Periodcally</option>
+                  <option>Other</option>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            </Row>
+            <Row>
+            <Col md={3}>
+              <Form.Group controlId="amount">
+                <Form.Label>Amount</Form.Label>
+                <Form.Control type="text" placeholder='Amount'>
+                  </Form.Control>
+              </Form.Group>
+              </Col>
+            <Col md={1}>
+              <Form.Group controlId="money">
+                <Form.Label>*</Form.Label>
+                <Form.Control as="select" placeholder='money'>
+                  <option>VND</option>
+                  <option>EUR</option>
+                  <option>USD</option>
+                  <option>POUND</option>
+                  </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col md={2}>
+            <Form.Label>per</Form.Label>
+            <Form.Control as="select" placeholder='per'>
+                  <option>month</option>
+                  <option>week</option>
+                  <option>day</option>
+                  </Form.Control>
+            </Col>
+            <Col md={5}>
+            <Form.Label>Custom Iteration</Form.Label>
+            <Form.Control type="text" placeholder='Custom Iteration/hours per day'>
+                  </Form.Control>
+            </Col>
+            </Row>
+            <Row><h3 className='header2'>Additional Information</h3></Row>
+            <Col>
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder='Enter Description'>
+                  </Form.Control>
+            </Col>
+            <Row>
+            <Col>
+            <Form.Label>Contact Info</Form.Label>
+            <Form.Control type="text" placeholder='Contact Info*'>
+                  </Form.Control>
+            </Col>
+            </Row>
+            
+           
+            
+          </Row>   
+          <Button type="submit" className='enlistbutton'>Enlist</Button>
+       </Form>
+       
+      </Container>
+      
+      <Footer />
+      
+    </>
   );
+
 }
 
 export default EnlistJob;
