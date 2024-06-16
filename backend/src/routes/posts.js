89bbc17419dd4cbbd252f8ffcad3049fc1c24622
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-06-16 08:44:10 +0700

    Update posts.js
    Add more queries to "post" and related tables.
    TODO: add to related table when update post

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -10,16 +10,39 @@ FROM post
 	JOIN content_history ON (content_id = post_id AND content_type = 0)
 WHERE post_status = 'PUBLISHED'
 ORDER BY post_id DESC;
-  `;
+`;
+
 const SELECT_POST_BY_ID = `
-SELECT post_id, post_title, post_content, username FROM post JOIN auth ON post.user_id = auth.user_id
+SELECT post_id, post_title, post_content, username, content_updated_time
+FROM post 
+	JOIN auth ON post.user_id = auth.user_id
+	JOIN content_history ON (content_id = post_id AND content_type = 0)
 WHERE post_status = 'PUBLISHED' AND post_id = @id
-ORDER BY post_id DESC;`;
-const INSERT_POST =
-  "INSERT INTO post (post_title, post_content, user_id, post_status) VALUES (@post_title, @post_content, @user_id, 'PUBLISHED')";
+ORDER BY post_id DESC;
+`;
+
+const INSERT_POST = `INSERT INTO post (post_title, post_content, user_id, post_status) 
+VALUES (@post_title, @post_content, @user_id, 'PUBLISHED');`;
+
+const DELETE_POST = `DELETE FROM post WHERE post_id = @post_id AND user_id = @user_id;`;
+
+const INSERT_CONTENT_HISTORY = `INSERT INTO content_history (content_type, content_id, content)
+VALUES (@content_type, @content_id, @content);`;
+
+const INSERT_FORUM_LOG = `INSERT INTO forum_log (content_type, content_id, content_history_id)
+VALUES (@content_type, @content_id, @content_history_id);`;
+
+const SELECT_COMMENTS_BY_POST_ID = `SELECT * FROM comment WHERE post_id = @post_id
+ORDER BY comment_id DESC;`;
+
+const INSERT_COMMENT = `INSERT INTO comment (comment_content, user_id, post_id, comment_status)
+VALUES (@comment_content, @user_id, @post_id, 'PUBLISHED');`;
+
+const DELETE_COMMENT = `DELETE FROM comment WHERE comment_id = @comment_id AND user_id = @user_id;`;
 
 const UPDATE_POST = `
-UPDATE post SET post_title = @post_title, post_content = @post_content
+UPDATE post 
+SET post_title = @post_title, post_content = @post_content
 WHERE post_id = @post_id AND user_id = @user_id;
 `;
 
@@ -33,7 +56,7 @@ router.get("/:id", async (req, res) => {
       .input("id", db.sql.Int, id)
       .query(SELECT_POST_BY_ID);
 
-    if (result.recordset.length === 0) {
+    if (result.recordset.length == 0) {
       res.status(404).json({ message: "Post not found" });
     } else {
       res.json(result.recordset[0]);
@@ -64,6 +87,18 @@ router.post("/", async (req, res) => {
       .input("post_content", db.sql.NVarChar, content)
       .input("user_id", db.sql.Int, user_id)
       .query(INSERT_POST);
+    const result2 = await pool
+      .request()
+      .input("content_type", db.sql.Int, 0)
+      .input("content_id", db.sql.Int, result.recordset[0].post_id)
+      .input("content", db.sql.NVarChar, content)
+      .query(INSERT_CONTENT_HISTORY);
+    const result3 = await pool
+      .request()
+      .input("content_type", db.sql.Int, 0)
+      .input("content_id", db.sql.Int, result.recordset[0].post_id)
+      .input("content_history_id", db.sql.Int, result2.recordset[0].content_id)
+      .query(INSERT_FORUM_LOG);
     res.status(201).json({ message: "Post inserted successfully" });
   } catch (err) {
     res.status(500).json({ message: "Error occurred", error: err });
@@ -90,9 +125,9 @@ router.put("/update", async (req, res) => {
     const result = await pool
       .request()
       .input("post_id", db.sql.Int, post_id)
+      .input("user_id", db.sql.Int, user_id)
       .input("post_title", db.sql.NVarChar, title)
       .input("post_content", db.sql.NVarChar, content)
-      .input("user_id", db.sql.Int, user_id)
       .query(UPDATE_POST);
 
     if (result.rowsAffected[0] === 0) {
@@ -101,7 +136,7 @@ router.put("/update", async (req, res) => {
       return res.status(200).json({ message: "Post updated successfully" });
     }
   } catch (err) {
-    return res.status(500).json({ message: "Error occurred", error: err });
+    return res.status(500).json({ message: "Error occurred", error: `${post_id}, ${user_id}, ${title}, ${content}` });
   }
 });
 
