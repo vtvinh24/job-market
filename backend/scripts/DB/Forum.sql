6f6b3bc29ed8119cf4893eaed90a5d557dd89dd4
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-06-20 07:35:07 +0700

    Update forum SQL queries

--- backend/scripts/DB/Forum.sql
+++ backend/scripts/DB/Forum.sql
@@ -1,55 +1,73 @@
 USE mJOB;
+
+GO
+CREATE TABLE
+	post (
+		post_id INT IDENTITY (1, 1) PRIMARY KEY,
+		post_title NVARCHAR (255) NOT NULL,
+		post_content NVARCHAR (MAX) NOT NULL,
+		user_id INT NOT NULL FOREIGN KEY REFERENCES auth (user_id),
+		post_status VARCHAR(16) NOT NULL DEFAULT 'PUBLISHED', -- DRAFT, PUBLISHED, ARCHIVED
+		post_created_time DATETIME NOT NULL DEFAULT GETDATE (),
+		post_updated_time DATETIME NOT NULL DEFAULT GETDATE (),
+		post_view_count INT NOT NULL DEFAULT 0
+	);
+
+GO
+INSERT INTO
+	system_log (system_log_type, system_log_data)
+VALUES
+	('INFO', 'Created table: post');
+
+GO
+CREATE TABLE
+	comment (
+		comment_id INT IDENTITY (1, 1) PRIMARY KEY,
+		user_id INT NOT NULL FOREIGN KEY REFERENCES auth (user_id),
+		post_id INT NOT NULL FOREIGN KEY REFERENCES post (post_id),
+		comment NVARCHAR (MAX),
+		comment_created_time DATETIME NOT NULL DEFAULT GETDATE (),
+		comment_updated_time DATETIME NOT NULL DEFAULT GETDATE (),
+	);
+
+GO
+INSERT INTO
+	system_log (system_log_type, system_log_data)
+VALUES
+	('INFO', 'Created table: comment');
+
 GO
 
-CREATE TABLE post (
-	post_id INT IDENTITY(1,1) PRIMARY KEY,
-	post_title NVARCHAR(255) NOT NULL,
-	post_content NVARCHAR(MAX) NOT NULL,
-	user_id INT NOT NULL FOREIGN KEY REFERENCES auth(user_id),
-	post_status VARCHAR(16) NOT NULL
-);
-GO
+-- ON UPDATING POST, INSERT NEW CONTENT TO THIS TABLE (ALSO INSERT AT POST CREATION)
+CREATE TABLE
+	post_history (
+		post_history_id INT IDENTITY (1, 1) PRIMARY KEY,
+		post_id INT NOT NULL,
+		post_title NVARCHAR (MAX) NOT NULL,
+		post_content NVARCHAR (MAX) NOT NULL,
+		post_updated_time DATETIME NOT NULL DEFAULT GETDATE ()
+	);
 
-INSERT INTO system_log (system_log_type, system_log_data)
-VALUES ('INFO', 'Created table: post');
 GO
+INSERT INTO
+	system_log (system_log_type, system_log_data)
+VALUES
+	('INFO', 'Created table: post_history');
 
-CREATE TABLE comment (
-	comment_id INT IDENTITY(1, 1) PRIMARY KEY,
-	user_id INT NOT NULL FOREIGN KEY REFERENCES auth(user_id),
-	post_id INT NOT NULL FOREIGN KEY REFERENCES post(post_id),
-	comment_content NVARCHAR(MAX),
-	comment_status VARCHAR(16) NOT NULL
-);
 GO
+-- SAME RULES AS POST_HISTORY
+CREATE TABLE
+	comment_history (
+		comment_history_id INT IDENTITY (1, 1) PRIMARY KEY,
+		comment_id INT NOT NULL,
+		comment NVARCHAR (MAX) NOT NULL,
+		comment_updated_time DATETIME NOT NULL DEFAULT GETDATE ()
+	);
 
-INSERT INTO system_log (system_log_type, system_log_data)
-VALUES ('INFO', 'Created table: comment');
 GO
+INSERT INTO
+	system_log (system_log_type, system_log_data)
+VALUES
+	('INFO', 'Created table: comment_history');
 
-CREATE TABLE content_history (
-	content_history_id INT IDENTITY(1,1) PRIMARY KEY,
-	content_type BIT NOT NULL, -- 0 if Post, 1 if Comment
-	content_id INT NOT NULL,
-	content NVARCHAR(MAX) NOT NULL,
-	content_updated_time DATETIME NOT NULL DEFAULT GETDATE()
-);
-GO
-
-INSERT INTO system_log (system_log_type, system_log_data)
-VALUES ('INFO', 'Created table: content_history');
-GO
-
-CREATE TABLE forum_log (
-	forum_log_id INT IDENTITY(1,1) PRIMARY KEY,
-	content_type BIT NOT NULL, -- 0 if Post, 1 if Comment
-	content_id INT NOT NULL,
-	content_history_id INT NOT NULL FOREIGN KEY REFERENCES content_history(content_history_id),
-	forum_log_type VARCHAR(16) NOT NULL, -- create, edit, delete, archive, ...
-	forum_log_time DATETIME DEFAULT GETDATE()
-);
-GO
-
-INSERT INTO system_log (system_log_type, system_log_data)
-VALUES ('INFO', 'Created table: forum_log');
 GO
